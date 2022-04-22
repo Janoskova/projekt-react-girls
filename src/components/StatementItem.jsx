b52f0d6acf395f82b596b9cd15311ff29c646228
Eva Janoskova <janoskova.evca@gmail.com>
2022-04-22 19:31:18 +0200

    buttons added to StatementItem

--- src/components/StatementItem.jsx
+++ src/components/StatementItem.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const StatementItem = ({ text, answer }) => {
-  return <div className="grid__item ">{text}</div>;
+  return (
+    <div className="grid__item">
+      {text}
+      <div className="grid__options">
+        <button className="grid__button">ano</button>
+        <button className="grid__button">ne</button>
+      </div>
+    </div>
+  );
 };
 
 export default StatementItem;
